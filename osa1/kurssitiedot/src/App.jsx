61a47c40bcaf01e6bd09f582e858f8a4bd79e72e
valpyots <valtteri.pyotsia@gmail.com>
2024-01-09 10:56:06 +0200

    osa 1.2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -11,9 +11,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
@@ -27,9 +25,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-    {props.part} {props.exercises}
-    </p>
+    <div>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
   )
 }
 
@@ -39,4 +39,11 @@ const Total = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+    {props.part} {props.exercises}
+    </p>
+  )
+}
 export default App
