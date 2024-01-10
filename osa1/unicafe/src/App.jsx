e9a97b43eb6d4d157c5261bab1b59031cec60514
valpyots <valtteri.pyotsia@gmail.com>
2024-01-10 17:05:03 +0200

    osa 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,13 +5,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad;
+  const avg = ((good * 1) + (neutral * 0) + (bad * -1)) / all;
+  const pos = good / all * 100;
 
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button><button onClick={() => setNeutral(neutral + 1)}>neutral</button><button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      good {good}<br />neutral {neutral}<br />bad {bad}
+      good {good}<br />neutral {neutral}<br />bad {bad}<br/>all {all}<br />average {avg}<br />positive {pos}%
     </div>
   )
 }
