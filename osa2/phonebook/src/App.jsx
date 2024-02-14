dd57924847e5a27afb94c4e40c798267dac4d06b
valpyots <valtteri.pyotsia@gmail.com>
2024-02-14 10:46:41 +0200

    osa 2.6

---
+++ osa2/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: newName,
+    }
+
+  setPersons(persons.concat(personObject))
+  setNewName('')
+  }
+
+  const handleNewPerson = (event) => {
+    setNewName(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewPerson}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(persons =>
+      <div key={persons.id}>{persons.name} </div>
+      )}
+    </div>
+  )
+}
+
+export default App
