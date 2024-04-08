bbf9fcd372a0d138933e3c16b1419e99584b3efa
valpyots <valtteri.pyotsia@gmail.com>
2024-04-08 12:21:18 +0300

    osa 2.8

--- osa2/phonebook/src/App.jsx
+++ osa2/phonebook/src/App.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
+      number: newNumber,
       name: newName,
       id: newName,
     }
@@ -14,11 +16,15 @@ const App = () => {
   } else {
   setPersons(persons.concat(personObject))
   setNewName('')
+  setNewNumber('')
   }
 }
 
   const handleNewPerson = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
   return (
     <div>
@@ -26,6 +32,7 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewPerson}/>
+          number: <input value={newNumber} onChange={handleNewNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -33,7 +40,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map(persons =>
-      <div key={persons.id}>{persons.name} </div>
+      <div key={persons.id}>{persons.name} {persons.number} </div>
       )}
     </div>
   )
